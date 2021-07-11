5fe1526fb576e9207687def6e10beff171f4769b
Anfaza <a.frayhi@gmail.com>
2021-07-11 19:31:40 +0200

    test for travis

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Page not found</title>
+</head>
+<body>
+<section style="padding-top:100px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 text-center">
+                <img src="https://th.bing.com/th/id/R.e96520439e9867e64a7164fee0a48a01?rik=Mh5VbIm%2f7ulLfA&amp;riu=http%3a%2f%2fgifimage.net%2fwp-content%2fuploads%2f2017%2f08%2fsearching-gif-5.gif&amp;ehk=I0C0JQjzTtWm8XKpgaJUoGQ%2fz3wvVrFSv1JeQPOsk7A%3d&amp;risl=&amp;pid=ImgRaw" alt="Afbeelding van visuele zoekopdracht" class=" nofocus" tabindex="0" aria-label="Afbeelding van visuele zoekopdracht">
+                <h1 style ="font-size: 100px;">404</h1>
+                <h2>Page not found</h2>
+
+                <p>Sorry,the page you requested could not be found. Please return to login.</p>
+                <a href="/login" class = "btn btn-primary">Return to login</a>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>500 internal server error</title>
+</head>
+<section style="padding-top:100px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 text-center">
+                <img src="https://c.tenor.com/TbQVd1OyxswAAAAi/robot-focused.gif" width="150" height="150" alt="Robot Focused Sticker - Robot Focused Watching Stickers" style="max-width: 424px; background-color: unset; margin: 8px;">
+                <img src="https://c.tenor.com/8XDC43z6VygAAAAi/robot-angry.gif" width="150" height="150" alt="Robot Angry Sticker - Robot Angry Annoyed Stickers" style="max-width: 424px; background-color: unset; margin: 8px;">
+                <h1 style ="font-size: 100px;">500</h1>
+                <h2>Internal server error</h2>
+
+                <h6>Sorry, at the moment we can not communicate. </h6>
+                <h8>Please contact your administrator and try again later.</h8>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
